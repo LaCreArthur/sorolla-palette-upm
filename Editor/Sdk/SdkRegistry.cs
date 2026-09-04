43d4a6e69127131660aea453c02301fa69adef1e
LaCreArthur <le-tutur@hotmail.fr>
2026-09-04 12:39:18 +0200

    fix: require Unity 6.5-compatible GameAnalytics

--- Editor/Sdk/SdkRegistry.cs
+++ Editor/Sdk/SdkRegistry.cs
@@ -105,7 +105,7 @@ namespace Sorolla.Palette.Editor
         // VERSION CONSTANTS - Update these when upgrading SDK versions
         // ============================================================
         public const string EDM_VERSION = "1.2.187";
-        public const string GA_VERSION = "8.0.1";
+        public const string GA_VERSION = "8.2.0";
         public const string MAX_VERSION = "8.6.4";
         public const string FB_VERSION = "18.0.1";
         public const string FIREBASE_VERSION = "13.7.0";
